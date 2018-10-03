78f08360935641f680f66252b76fdf806fca0171
Jacyln Kan <jacylnkan@gmail.com>
2018-10-02 19:44:52 -0600

    Added grocery list

---
+++ grocery.cpp
@@ -0,0 +1,14 @@
+//test.cpp
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	const char* groceries [10];
+	groceries [0] = "Apples";
+	groceries [1] = "Bananas";
+	groceries [2] = "Milk";
+	groceries [3] = "Vegetables";
+	cout << groceries[2];
+	return 0;
+}
